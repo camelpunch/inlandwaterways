a9d8e6f3eff3ae6e66e9bee00cbf121bea36bcb6
Andrew Bruce <andrew@camelpunch.com>
2015-01-11 23:33:37 +0000

    Port Google example to ClojureScript

--- src/inlandwaterways/core.cljs
+++ src/inlandwaterways/core.cljs
@@ -1,8 +1,18 @@
 (ns inlandwaterways.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl :as repl]
+            [goog.events :as events]))
 
 ;; (repl/connect "http://localhost:9000/repl")
 
 (enable-console-print!)
 
 (println "Hello world!")
+
+(def map-opts {:center { :lat -34.397 :lng 150.644 }
+               :zoom 8})
+
+(defn map-load []
+  (js/google.maps.Map. (.getElementById js/document "map-canvas")
+                       (clj->js map-opts)))
+
+(events/listen js/window "load" map-load)
