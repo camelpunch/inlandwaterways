7448e5546859fd4c692691db1865355ca26e6aff
Andrew Bruce <andrew@camelpunch.com>
2015-01-15 00:23:23 +0000

    Draw line from Nantwich to English Channel

--- src/inlandwaterways/core.cljs
+++ src/inlandwaterways/core.cljs
@@ -2,17 +2,24 @@
   (:require [clojure.browser.repl :as repl]
             [goog.events :as events]))
 
-;; (repl/connect "http://localhost:9000/repl")
-
 (enable-console-print!)
-
-(println "Hello world!")
 
 (def map-opts {:center { :lat 53.0672367 :lng -2.52393 }
                :zoom 8})
 
+(def coords [{:lat 53.0672367 :lng -2.5239}
+             {:lat 50 :lng -2.52}])
+
+(def polyline
+  (js/google.maps.Polyline. (clj->js {:path coords
+                                      :geodesic true
+                                      :strokeColor "#FF0000"
+                                      :strokeOpacity 1.0,
+                                      :strokeWeight 2})))
+
 (defn map-load []
-  (js/google.maps.Map. (.getElementById js/document "map-canvas")
-                       (clj->js map-opts)))
+  (let [map (js/google.maps.Map. (.getElementById js/document "map-canvas")
+                                 (clj->js map-opts))]
+    (.setMap polyline map)))
 
 (events/listen js/window "load" map-load)
